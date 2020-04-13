8b8c3f7bfedf81fefabe05366a7cd1213819edee
Alex Li <alelia40@gmail.com>
2020-04-13 19:03:17 -0400

    adds some home page components

--- news-app/src/app/app.module.ts
+++ news-app/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes, Router } from '@angular/router';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+import {SlideshowModule} from 'ng-simple-slideshow';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
@@ -13,6 +17,10 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { MapComponent } from './components/map/map.component';
+import { WeatherComponent } from './components/home/weather/weather.component';
+import { ImageSliderComponent } from './components/home/image-slider/image-slider.component';
+import { NewsComponent } from './components/home/news/news.component';
+import { ChatComponent } from './components/home/chat/chat.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
@@ -33,13 +41,20 @@ const routes: Routes = [
     ContactUsComponent,
     PageNotFoundComponent,
     FooterComponent,
-    MapComponent
+    MapComponent,
+    WeatherComponent,
+    ImageSliderComponent,
+    NewsComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    SlickCarouselModule,
+    IvyCarouselModule,
+    SlideshowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
