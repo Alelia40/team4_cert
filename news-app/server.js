fbe2ca008a05062fa6d5451ec0a489df2d8687d1
Alex Li <alelia40@gmail.com>
2020-04-15 13:01:13 -0400

    completes function of chat box

--- news-app/server.js
+++ news-app/server.js
@@ -8,3 +8,12 @@ const PORT = 3000
 const server = http.createServer(app)
 
 server.listen(PORT, () => console.log('Server Started on PORT ' + PORT))
+
+let io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+  socket.on('chat', (message) =>{
+    io.emit("Chat Message", message);
+  });
+});
